e17cda3a4e03feed5edf00334ac5f0fea9f45d0d
John Vasconcelos <39773960+Johnvasc@users.noreply.github.com>
2021-12-21 23:06:41 -0300

    Update Geranome.cpp

--- Geranome.cpp
+++ Geranome.cpp
@@ -10,25 +10,28 @@ char vogal(){
 }
 char cons(){
     char consul[] = {'b', 'c','d','f','g','h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't','v', 'x', 'z' };
-    int n = rand() % 20;
+    int n = rand() % 19;
     return consul[n];
 }
 
 
 int main(){
-    int tNome = 4;
+    int tNome = 6;
     char nome[tNome];
     int opcao = 1;
     do{
         bool cAchada = false;
+        int nVogal = 0;
         for(int j = 0; j<tNome; j++){
-            if(rand()%2 && !cAchada){
+            if((rand()%2 && !cAchada) || nVogal==2){
                 nome[j] = cons();
                 cAchada = true;
+                nVogal = 0;
             }
             else{
                 nome[j] = vogal();
                 cAchada = false;
+                nVogal++;
             }
         }
         for(int i = 0; i<tNome; i++){
